fe238a8d6c27e2041662b3ffae3a3e69a37f0c54
Micrah <44911744+milestorme@users.noreply.github.com>
2019-11-22 05:18:18 +1100

    update

--- src/pocketportal.cpp
+++ src/pocketportal.cpp
@@ -37,8 +37,8 @@ public:
         player->PlayerTalkClass->ClearMenus();
 
         if (sConfigMgr->GetBoolDefault("PocketPortal.Enable", true))
-            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_INTERACT_2, "|TInterface/ICONS/INV_Misc_Map08:35:35:-23:0|t-> Pocket Portal", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-            player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, item->GetGUID());
+          player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Pocket Portal", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+          player->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, item->GetGUID());
         return false; // Cast the spell on use normally
     }
 void OnGossipSelect(Player* player, Item* /*item*/, uint32 /*sender*/, uint32 action) override
