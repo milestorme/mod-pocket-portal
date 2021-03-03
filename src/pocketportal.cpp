82c6a60b76751ddcd98a371cc1531dadff035425
Vitaliy <mitradis@gmail.com>
2021-03-03 22:26:21 +0300

    fix for last PR ACE remove (#4)
    https://github.com/azerothcore/azerothcore-wotlk/pull/4608

--- src/pocketportal.cpp
+++ src/pocketportal.cpp
@@ -91,49 +91,7 @@ void OnGossipSelect(Player* player, Item* /*item*/, uint32 /*sender*/, uint32 ac
     }
 };
 
-
-class pportal_world : public WorldScript
-{
-public:
-    pportal_world() : WorldScript("pportal_world") { }
-
-    void OnBeforeConfigLoad(bool reload) override
-    {
-        if (!reload) 
-        {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/pocketportal.conf";            
-            
-#if PLATFORM == PLATFORM_WINDOWS
-            cfg_file = "pocketportal.conf";
-#endif
-            std::string cfg_def_file = cfg_file + ".dist";
-            
-            // Load .conf.dist config
-            if (!sConfigMgr->LoadMore(cfg_def_file.c_str()))
-            {
-                sLog->outString();
-                sLog->outError("Module config: Invalid or missing configuration dist file : %s", cfg_def_file.c_str());
-                sLog->outError("Module config: Verify that the file exists and has \'[worldserver]' written in the top of the file!");
-                sLog->outError("Module config: Use default settings!");
-                sLog->outString();
-            }
-            
-            // Load .conf config
-            if (!sConfigMgr->LoadMore(cfg_file.c_str()))
-            {
-                sLog->outString();
-                sLog->outError("Module config: Invalid or missing configuration file : %s", cfg_file.c_str());
-                sLog->outError("Module config: Verify that the file exists and has \'[worldserver]' written in the top of the file!");
-                sLog->outError("Module config: Use default settings!");
-                sLog->outString();
-            }
-        }
-    }
-};
-
 void AddSC_pocket_portal()
 {
     new pocket_portal();
-    new pportal_world();
 }
